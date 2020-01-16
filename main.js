9321271f2955e9586aba3d1ccb295f606636163b
Davide Lugli <luglidavide.web@gmail.com>
2020-01-16 15:33:33 +0100

    Fine
    metodo con var??

--- main.js
+++ main.js
@@ -1,25 +1,37 @@
-// $('i.fa-bars').click(
-//   function () {
-//     $('.hamburger-menu').show('fast');
-//   }
-// );
 
-var hidden = true;
-if (hidden) {
+// con var hidden, non mi viene
+// var hidden = true;
+// console.log(hidden);
+// if (hidden) {
+//   $('i.fa-bars').click(
+//     function() {
+//       $('.hamburger-menu').show('fast');
+//       hidden = false;
+//       console.log(hidden);
+//     }
+//   );
+// } else if (hidden == false) {
+//   $('i.fas.fa-times').click(
+//     function() {
+//       $('.hamburger-menu').hide('fast');
+//       hidden = true;
+//     }
+//   );
+// }
+
+
+
   $('i.fa-bars').click(
     function() {
       $('.hamburger-menu').show('fast');
       hidden = false;
       console.log(hidden);
     }
-
   );
-} else if (!hidden) {
+
   $('a.close').click(
     function() {
       $('.hamburger-menu').hide('fast');
       hidden = true;
     }
-
   );
-}
