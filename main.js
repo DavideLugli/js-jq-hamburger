ab7f01b49ebd4566c4ae7f66fbedc1f761bbdeb9
Davide Lugli <luglidavide.web@gmail.com>
2020-01-16 15:15:08 +0100

    Si apre ma non si chiude (ci sto lavorando)

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
-alert('ciao');
+// $('i.fa-bars').click(
+//   function () {
+//     $('.hamburger-menu').show('fast');
+//   }
+// );
+
+var hidden = true;
+if (hidden) {
+  $('i.fa-bars').click(
+    function() {
+      $('.hamburger-menu').show('fast');
+      hidden = false;
+      console.log(hidden);
+    }
+
+  );
+} else if (!hidden) {
+  $('a.close').click(
+    function() {
+      $('.hamburger-menu').hide('fast');
+      hidden = true;
+    }
+
+  );
+}
